c5d17fce09128df883f42d489dee5a65da16a080
Tolulope_xo <60390674+Tolulope-xo@users.noreply.github.com>
2022-03-18 09:01:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.burger').click(function(){
+        $('ul').toggleClass('active');
+    })
+})
+
+
+
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('ul');
+
+//     burger.addEventListener('click',()=>{
+//         nav.classList.toggle('ul:active');
+//     });
+// }
+
+// navSlide();
